f6f3958a9401d65377848c061646ae8e07b9b85c
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2023-09-28 00:25:08 -0400

    setup third fragment

--- app/src/main/java/com/sujeevuthayakumar/sofe4650_assignment1/ThirdFragment.java
+++ app/src/main/java/com/sujeevuthayakumar/sofe4650_assignment1/ThirdFragment.java
@@ -1,6 +1,27 @@
 package com.sujeevuthayakumar.sofe4650_assignment1;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
+import com.sujeevuthayakumar.sofe4650_assignment1.databinding.FragmentFirstBinding;
+import com.sujeevuthayakumar.sofe4650_assignment1.databinding.FragmentThirdBinding;
+
 public class ThirdFragment extends Fragment {
+
+    private FragmentThirdBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentThirdBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
 }
